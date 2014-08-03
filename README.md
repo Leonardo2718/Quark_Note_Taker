4271f2a11b01dc084c6c211816dd3882e0a61f77
Leonardo Banderali <lbanderali@yahoo.com>
2014-08-03 15:28:09 -0600

    Initial commit.  Create basic stub code to eventually replace it with actual working code. On branch master (initial commit), changes to be committed: 	new file:   .gitignore 	new file:   README.md 	new file:   config.json 	new file:   html-template/MathJax 	new file:   html-template/htmlDoc_end.html 	new file:   html-template/htmlDoc_start.html 	new file:   html-template/stylesheet.css 	new file:   mainwindow.py 	new file:   quark.py 	new file:   quarkExtra.py

---
+++ README.md
@@ -0,0 +1,8 @@
+#Quark Note Taker
+Quark Note Taker is an opensource note taking application, written in python.
+Notes are stored as plain-text/markdown files.  Some of its features *will* include:
+markdown live preview, markdown syntax highlighting, mathjax support, and simple note management.  
+Quark is highly customizable as all files (including config and source files) are editable by the user.
+
+##License
+Quark  Note Taker is licensed under the MIT License.
---
+++ quark.py
@@ -0,0 +1,88 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Project: Quark Note Taker
+File: quark.py
+Author: Leonardo Banderali
+Created: August 3, 2014
+Last Modified: August 3, 2014
+
+Description: 
+	Quark Note Taker is an opensource note taking application.  Notes are stored
+	as plain-text/markdown files.  Some of its features include: markdown live preview,
+	markdown syntax highlighting, mathjax support, and simple note management.  Quark is 
+	highligh custumizable as all files (including config and source files) are editable 
+	by the user.
+
+
+Copyright (C) 2014 Leonardo Banderali
+
+License:
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+"""
+
+#~import modules~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#python modules
+import sys
+import os
+
+#Qt5 modules
+from PyQt5.QtWidgets import QApplication
+
+#Quark specific modules
+from mainwindow import MainWindow
+
+
+
+#~main code~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if __name__ == '__main__':
+
+	#create Quark application
+	app = QApplication(sys.argv)
+	win = MainWindow()
+
+	#print copyright statement
+	print("""
+Copyright (C) 2014 Leonardo Banderali
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+	""")
+
+	#execute the app
+	win.show()
+	app.exec_()
